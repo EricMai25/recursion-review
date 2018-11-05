75db9cdb5899c9b0e486dc14cba12e588d94902c
Hack Reactor Student <student@hackreactor.com>
2018-11-05 15:57:18 -0800

    update

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,49 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var array = [];
+
+  var domBody = document.getElementsByTagName('body');
+
+  // if(domBody.length === 0) {
+
+  // } else {
+    
+  // }
+  var run = function(element) {
+    
+    if (element[0].classList.length > 0) {
+      for (var j = 0; j < element[0].classList.length; j++) {
+        if (element[0].classList[j] === className) {
+          array.push(element[0]);
+        }
+      }
+
+
+    }
+    if (element[0].children.length > 0) {
+      for (i = 0; i < element[0].children.length; i++) {
+        run(element[0].children[i].children);
+      }
+    }
+    return array; 
+
+   
+  
+
+
+
+
+  // input: className 
+
+  // iterating thru the document body 
+  // 
+
+  // output: all elements with className 
+  };
+  
+  run(domBody);
+
 };
+
